dad68478bab91dc86f560d861ce14060fbfa5917
0NOVEMBER0 <111534907+0NOVEMBER0@users.noreply.github.com>
2026-07-07 21:40:28 +0200

    Add files via upload

--- Emerytura.py
+++ Emerytura.py
@@ -8,21 +8,13 @@ sex = input("PłEĆ (WPISZ M LUB K): \n")
 
 if (sex.upper() == "K"):
     czas_do_emerytury = 60-(current_year-year_of_birth)
-    if (czas_do_emerytury > 10):
-        print(
-            f"{name}, zostało Ci {czas_do_emerytury} lat do emerytury, trochę się naczekasz")
-    if (0 < czas_do_emerytury <= 10):
-        print(
-            f"{name}, zostało Ci {czas_do_emerytury} lat do emerytury, już niewiele, keep going!")
     if (czas_do_emerytury <= 0):
         print("Farciara, już na wolnym --")
 elif (sex.upper() == "M"):
     czas_do_emerytury = 65-(current_year-year_of_birth)
-    if (czas_do_emerytury > 10):
-        print(
-            f"{name}, zostało Ci {czas_do_emerytury} lat do emerytury, trochę się naczekasz")
-    if (0 < czas_do_emerytury <= 10):
-        print(
-            f"{name}, zostało Ci {czas_do_emerytury} lat do emerytury, już niewiele, keep going!")
     if (czas_do_emerytury <= 0):
         print("Farciarz, już na wolnym --")
+if (czas_do_emerytury > 10):
+    print(f"{name}, zostało Ci {czas_do_emerytury} lat do emerytury, trochę się naczekasz")
+if (0 < czas_do_emerytury <= 10):
+    print(f"{name}, zostało Ci {czas_do_emerytury} lat do emerytury, już niewiele, keep going!")
